3d1f5cb7a49ddc344de0c2b46e6d14715a87a0bd
Daniel Kurui <Daniel Kurui>
2015-10-28 15:08:32 +0300

    what i have added

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="cs" lang="cs">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>GENUX PARCEL SERVICES</title>
+{{HTML::style}}
+<link rel="stylesheet" media="screen,projection" type="text/css" href="css/main.css" />
+<link rel="stylesheet" media="print" type="text/css" href="css/print.css" />
+<link rel="stylesheet" media="aural" type="text/css" href="css/aural.css" />
+</head>
+<body id="www-url-cz">
+<div id="main" class="box">
+  <div id="header">
+    <h1 id="logo"><strong><a href="#">GENUX PARCEL SERVICE</strong><span></span></a></h1>
+    <hr class="noscreen" />
+    <div class="noscreen noprint">
+      <p><em>Quick links: <a href="#content">content</a>, <a href="#tabs">navigation</a>, <a href="#search">search</a>.</em></p>
+      <hr />
+    </div>
+    <div id="search" class="noprint">
+      <form action="#" method="get">
+        <fieldset>
+        <legend>Search</legend>
+        <label><span class="noscreen">Find:</span> <span id="search-input-out">
+        <input type="text" name="" id="search-input" size="30" />
+        </span></label>
+        <input type="image" src="design/search_submit.gif" id="search-submit" value="OK" />
+        </fieldset>
+      </form>
+    </div>
+  </div>
+  <div id="tabs" class="noprint">
+    <h3 class="noscreen">Navigation</h3>
+    <ul class="box">
+      <li><a href="#">Home<span class="tab-l"></span><span class="tab-r"></span></a></li>
+      <li id="active"><a href="#">Weblog<span class="tab-l"></span><span class="tab-r"></span></a></li>
+      <li><a href="#">About Us<span class="tab-l"></span><span class="tab-r"></span></a></li>
+      <li><a href="#">Check your Parcel position<span class="tab-l"></span><span class="tab-r"></span></a></li>
+     
+    </ul>
+    <hr class="noscreen" />
+  </div>
+  <div id="page" class="box">
+    <div id="page-in" class="box">
+      <div id="strip" class="box noprint">
+        <p><strong><marquee>Welcome to Genux</marquee></strong></p>
+        <hr class="noscreen" />
+       
+      </div>
+      <div id="content">
+        <div class="article">
+          <h2><span>WELCOME TO GENUX PARCEL SERVICE</span></h2>
+          <p class="info noprint"> <span class="date"> <script language="javascript">
+                                                          var today = new Date();
+                                                              document.write(today);
+                                                         </script>
+                                    </span> 
+          </p>
+          <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aliquam pellentesque enim blandit enim bibendum blandit. Integer eu leo ac est aliquet imperdiet. Quisque nec justo id augue posuere malesuada. Nullam ac metus. Cras non leo ut est placerat condimentum. Aliquam ut enim. Quisque non sapien in enim eleifend faucibus. Pellentesque sodales. Mauris auctor arcu sit amet felis. Donec eget enim ut lacus pharetra condimentum. Nulla in felis vel tortor imperdiet consectetuer. Sed id ante.</p>
+          {!! Form::open(array('url' => 'foo/bar')) !!}
+        </div>
+       
+      </div>
+      <div id="col" class="noprint">
+        <div id="col-in">
+          <h3><span>GET TO ADMIN PANEL</span></h3>
+          <div id= "admin ">
+         
+           
+           <p class="btn-more box noprint"><strong><a href="#">ADMIN LOGIN</a></strong></p>
+ 
+          </div>
+          <hr class="noscreen" />
+          <h3 ><span>OUR VALUES</span></h3>
+          <ul id="category">
+            <li id="category-active"><a href="#">Safety</a></li>
+            <li id="category-active"><a href="#">Swiftness</a></li>
+            <li id="category-active"><a href="#">Notified</a></li>
+           
+          </ul>
+          
+        </div>
+      </div>
+    </div>
+  </div>
+  <div id="footer">
+    <div id="top" class="noprint">
+     <!-- <p><span class="noscreen">Back on top</span> <a href="#header" title="Back on top ^">^<span></span></a></p> -->
+    </div>
+    <hr class="noscreen" />
+   
+    <p id="copyright">&copy; 2015 | Genux Parcel delivery system designed by <a href="https://www.linkedin.com/profile/view?id=AAIAABpXvLoBKSldip795___GA93lq2h3VsWV68&trk=nav_responsive_tab_profile_pic" target="_blank">Kurui Daniel</a> </p>
+  </div>
+</div>
+</body>
+</html>
